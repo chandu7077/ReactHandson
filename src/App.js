010782f4ca7aee1a9b7be2302d4b2402e9b376b5
Chandu <411613@student.nitandhra.ac.in>
2021-02-09 18:25:16 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
 import "./styles.css";
+import CalculatorScore from "../src/Components/CalculatorScore";
 export default function App() {
-  return <div className="container"></div>;
+  return (
+    <div>
+      <CalculatorScore
+        Name={"Chandu"}
+        School={"Nava Jyothi High School"}
+        Total={576}
+        Goal={6}
+      />
+    </div>
+  );
 }
